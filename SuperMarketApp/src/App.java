1ccf761f47375b06c58a50d6c641bc291b1df670
Netkachev <mnetckachev@yandex.ru>
2023-08-31 09:12:51 +0300

    New class ActionClient

--- SuperMarketApp/src/App.java
+++ SuperMarketApp/src/App.java
@@ -1,22 +1,27 @@
-import Classes.Actor;
-import Classes.Market;
-import Classes.OrdinaryClient;
-import Classes.SpecialClient;
-import Classes.TaxInspector;
+import Classes.*;
 import Interfaces.iActorBehaviour;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Market magnit = new Market();
+        ActionClient.setAction("Второй апельсин в подарок");
+        ActionClient.setMaxActionCount(2);
 
         iActorBehaviour client1 = new OrdinaryClient("boris");
         iActorBehaviour client2 = new SpecialClient("prezident", 1);
         iActorBehaviour client3 = new TaxInspector();
+        iActorBehaviour client4 = new ActionClient("Ivan");
+        iActorBehaviour client5 = new ActionClient("Sergey");
 
         magnit.acceptToMarket(client1);
         magnit.acceptToMarket(client2);
         magnit.acceptToMarket(client3);
 
+        magnit.acceptToMarket(client4);
+        magnit.acceptToMarket(client5);
+
         magnit.update();
+
+        System.out.printf("Количество покупателей, обслуженных по акции \"%s\": %d", ActionClient.getAction(), ActionClient.getActionCount());
     }
 }
---
+++ SuperMarketApp/src/Classes/ActionClient.java
@@ -0,0 +1,90 @@
+package Classes;
+
+/**
+ * @apiNote
+ */
+public class ActionClient extends Actor {
+    private static String action;
+    private int id;
+    private static int maxActionCount;
+    private static int actionCount;
+    private static int currentId;
+
+    static {
+        currentId = 0;
+        actionCount = 0;
+        maxActionCount = 0;
+    }
+    /**
+     * @apiNote Set action name
+     * @param action action name
+     */
+    public static  void setAction(String action) {
+        ActionClient.action = action;
+    }
+
+    /**
+     * @apiNote Returns action name
+     * @return
+     */
+    public static String getAction() {
+        return ActionClient.action;
+    }
+
+    /**
+     * @apiNote Set maximum count of action clients
+     * @param newMaxActionCount count of action clients
+     */
+    public static void setMaxActionCount(int newMaxActionCount) {
+        ActionClient.maxActionCount = newMaxActionCount;
+    }
+
+    /**
+     * @apiNote Количество покупателей, обслуженных по акции
+     * @return
+     */
+    public static int getActionCount() {
+        return actionCount;
+    }
+
+    public ActionClient(String name) {
+        super(name);
+        this.id = ++currentId;
+    }
+
+    @Override
+    public void setName(String name) {
+        super.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return super.name;
+    }
+
+    @Override
+    public boolean isTakeOrder() {
+        return super.isTakeOrder;
+    }
+
+    @Override
+    public boolean isMakeOrder() {
+        return super.isMakeOrder;
+    }
+
+    @Override
+    public void setTakeOrder(boolean take) {
+        ActionClient.actionCount++;
+        super.isTakeOrder = take;
+    }
+
+    @Override
+    public void setMakeOrder(boolean make) {
+        super.isMakeOrder = make;
+    }
+
+    @Override
+    public Actor getActor() {
+        return this;
+    }
+}
